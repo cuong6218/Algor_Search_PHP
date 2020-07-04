cce905a16a405219403c9051a77ba05820ad2c20
cuong6218 <cuong6218@domain.com>
2020-07-04 18:21:44 +0700

    update function random phone number

--- ClassifyPhone/ClassifyPhone.php
+++ ClassifyPhone/ClassifyPhone.php
@@ -1,28 +1,26 @@
 <?php
     include_once ('PhoneNumber.php');
     
-    function createPhoneNumber($subPhone){
-        $sub = rand(0,17);
-        $number = rand(1000000, 9999999);
-        $phone = new PhoneNumber($subPhone[$sub], $number);
-        return $phone->getSubNumber().$phone->getNumber();
-    }
-    function createMultiNumber($subPhone){
+    function createMultiNumber($phone){
         $numbers = [];
         for ($i = 0; $i < 10; $i++){
-            array_push($numbers, createPhoneNumber($subPhone));
+            array_push($numbers, $phone);
         }
         return $numbers;
     }
 
-    $phoneNumber = 10;
+    // $phoneNumber = 10 number;
     $viettel = ['032', '033', '034', '035', '036', '037', '038', '039'];
     $mobi = ['070', '076', '077', '078', '079'];
     $vina = ['081', '082', '083', '084', '085'];
     $subPhone = array_merge($viettel, $mobi, $vina);
 
-    $numbers = createMultiNumber($subPhone);
+    $sub = rand(0,17);
+    $number = rand(1000000, 9999999);
+    $phone = new PhoneNumber($subPhone[$sub], $number);
+
+    $numbers = createMultiNumber($phone);
     echo '<pre>';
     print_r($numbers);
     echo '</pre>';
-    
+    // echo $phone->getSubNumber();
