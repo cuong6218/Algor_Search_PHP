5663e8de7b298dec90ee284e4172c5b728f31e69
cuong6218 <cuong6218@domain.com>
2020-07-06 15:17:06 +0700

    add solution two

--- GuessNumber/GuessNumber.php
+++ GuessNumber/GuessNumber.php
@@ -1,22 +1,21 @@
 <?php
-    
-    function guessNumber($number){
-        $numbers = range(1,100);
-        $low = 0;
-        $high = count($numbers) - 1;
-        while ($low <= $high){
-            $mid = floor((($low + $high)/2) ) ;
-            if ($numbers[$mid] > $number){
-                $high = $mid -1;
-                echo '<br/>Computer Number '.$numbers[$mid]. ' too high';
-            } else if ($numbers[$mid] < $number){
-                $low = $mid + 1;
-                echo '<br/>Computer Number '.$numbers[$mid]. ' too low';
-            }else  return '<br/>Guess number is '.$numbers[$mid];
-            
-        } 
-        
-        
+
+function guessNumber($number)
+{
+    $numbers = range(1, 100);
+    $low = 0;
+    $high = count($numbers) - 1;
+    while ($low <= $high) {
+        $mid = floor((($low + $high) / 2));
+        if ($numbers[$mid] > $number) {
+            $high = $mid - 1;
+            echo '<br/>Computer Number ' . $numbers[$mid] . ' too high';
+        } else if ($numbers[$mid] < $number) {
+            $low = $mid + 1;
+            echo '<br/>Computer Number ' . $numbers[$mid] . ' too low';
+        } else  return '<br/>Guess number is ' . $numbers[$mid];
     }
-    $number = rand(1,100);
-    echo guessNumber($number);
+}
+$number = rand(1, 100);
+echo guessNumber($number);
+?>
