8d11b8a77e8850b45f8dbcd25ac615228a2da4e3
cuong6218 <cuong6218@domain.com>
2020-07-07 10:32:02 +0700

    add file test

---
+++ test/index.php
@@ -0,0 +1,39 @@
+<?php
+//   $a = 5;
+//   $b = 10;
+// function increase()
+// {
+//     global $a;
+//     global $b;
+//     $a += ++$a + $b--;
+//     // return $a;
+// }
+// increase();
+// echo $a; //21
+// echo "<br/>";
+// echo $b;//9
+// $x = 3;
+// $y = 4;
+// $x += ++$x + $y--;
+// echo $x."<br/>";
+// echo $y;
+// class Name{
+//     public $name;
+//     function __construct($name)
+//     {
+//         $this->name = $name;
+//     }
+//     function __toString()
+//     {
+//         return 'Name1 to string '.$this->name."<br/>";
+//     }
+// }
+// $trung = new Name('Trung');
+// $cuc = $trung;
+// $cuc->name = 'Cuc';
+
+// $cuc = null;
+// echo $trung->__toString();
+// echo $trung;
+// echo $cuc;
+
